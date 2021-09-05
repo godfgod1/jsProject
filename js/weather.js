856fbdeb45a902317dac6abd720f8daa64d5d333
godfgod1 <godfgod1@naver.com>
2021-09-05 23:27:01 +0900

    done

---
+++ js/weather.js
@@ -0,0 +1,18 @@
+const weather = document.querySelector("#weather span:first-child");
+const API_KEY = "407892dbe7f75744fd9589329216da0c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      weather.innerText = `날씨 ${data.weather[0].main}  / ${data.main.temp} 도`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
